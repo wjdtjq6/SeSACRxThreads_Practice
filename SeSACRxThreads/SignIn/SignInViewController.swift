d0848530a2f13e4f27443017df1c8219c5238169
jimmy <wjdtjq6@gmail.com>
2024-08-03 22:26:20 +0900

    impleted SignInViewController and All functionn

--- SeSACRxThreads/SignIn/SignInViewController.swift
+++ SeSACRxThreads/SignIn/SignInViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class SignInViewController: UIViewController {
 
@@ -14,7 +16,10 @@ class SignInViewController: UIViewController {
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let signInButton = PointButton(title: "로그인")
     let signUpButton = UIButton()
-    
+    let emailRelay = BehaviorRelay<String>(value: "")
+    let passwordRelay = BehaviorRelay<String>(value: "")
+    let viewModel = SignInViewModel()
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,17 +28,60 @@ class SignInViewController: UIViewController {
         configureLayout()
         configure()
         
-        signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        bind()
     }
     
-    @objc func signUpButtonClicked() {
+    func bind() {
+        
+        let validationEmail = emailTextField.rx.text.orEmpty
+            .map({ $0.count > 0 })
+            
+        emailTextField.rx.text.orEmpty
+            .bind(to: emailRelay)
+            .disposed(by: disposeBag)
+                    
+        let validationPW = passwordTextField.rx.text.orEmpty
+            .map { $0.count > 0 }
+        
+        passwordTextField.rx.text.orEmpty
+            .bind(to: passwordRelay)
+            .disposed(by: disposeBag)
+
+        Observable.combineLatest(validationEmail, validationPW) { $0 && $1 }
+            .bind(with: self, onNext: { owner, value in
+                owner.signInButton.isEnabled = value
+                let color: UIColor = value ? .systemBlue : .black
+                owner.signInButton.backgroundColor = color
+            })
+            .disposed(by: disposeBag)
+        
+        signInButton.rx.tap
+            .subscribe { _ in
+                let email = self.emailRelay.value
+                let pw = self.passwordRelay.value
+                
+                if let isPW = self.viewModel.account[email], isPW == pw {
+                    self.showAlert(message: "로그인 성공!")
+                } else {
+                    self.showAlert(message: "이메일 또는 비밀번호가 잘못되었습니다.")
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let check = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(check)
+        present(alert, animated: true)
+    }
+    @objc func signUpButtonPressed() {
         navigationController?.pushViewController(SignUpViewController(), animated: true)
     }
-    
-    
     func configure() {
         signUpButton.setTitle("회원이 아니십니까?", for: .normal)
         signUpButton.setTitleColor(Color.black, for: .normal)
+        signUpButton.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside)
     }
     
     func configureLayout() {
--- SeSACRxThreads/SignIn/SignInViewModel.swift
+++ SeSACRxThreads/SignIn/SignInViewModel.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 class SignInViewModel {
-    
-    
-    
+    let account = [
+        "zk800@naver.com":"1234",
+        "zk800@gmail.com":"th1234"
+    ]    
 }
